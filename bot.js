9aaba8c7d5886db7df04c6f1c680881bba72554d
jmtkt8 <jmtkt8@mail.missouri.edu>
2016-07-22 22:53:49 -0500

    Deleted question mark

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^Conquistabot cool guy me$/;
-	  helloRegex = /^Conquistabot how are you?$/
+	  helloRegex = /^Conquistabot how are you$/
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
