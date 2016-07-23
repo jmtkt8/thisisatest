315eedd0167d7d0a041e36d0b1765f3cce4b6d27
jmtkt8 <jmtkt8@mail.missouri.edu>
2016-07-22 23:29:33 -0500

    Testing with array of gifs

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^Conquistabot cool guy me$/;
 	  helloRegex = /^Conquistabot how are you$/
+	  dogRegex = /^Conquistabot dog me$/
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
@@ -14,6 +15,10 @@ function respond() {
   }else if(request.text && helloRegex.test(request.text)){
     this.res.writeHead(200);
     postResp();
+    this.res.end();
+  }else if(request.text && dogRegex.test(request.text)){
+    this.res.writeHead(200);
+    dogMe();
     this.res.end();
   }else {
     console.log("don't care");
@@ -62,6 +67,49 @@ function postResp() {
   var botResponse, options, body, botReq;
 
   botResponse = 'I am alive!';
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
+function dogMe() {
+  var botResponse, options, body, botReq, rand;
+
+  rand = (Math.random() * 3) + 1
+  dogs = {
+	'http://i.imgur.com/VKCrLAF.gif',
+	'http://i.imgur.com/R8se5g1.gif',
+	'http://i.imgur.com/Bw3mIJF.gif'
+  }
+  
+  botResponse = dogs[rand];
 
   options = {
     hostname: 'api.groupme.com',
