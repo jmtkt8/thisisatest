1ca03539be33d935d8c9ec050fc136833c67412e
jmtkt8 <jmtkt8@mail.missouri.edu>
2016-07-22 22:51:07 -0500

    Added command

--- bot.js
+++ bot.js
@@ -6,12 +6,16 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^Conquistabot cool guy me$/;
-
+	  helloRegex = /^Conquistabot how are you?$/
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
-  } else {
+  }else if(request.text && helloRegex.test(request.text)){
+    this.res.writeHead(200);
+    postResp();
+    this.res.end();
+  }else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
@@ -22,6 +26,42 @@ function postMessage() {
   var botResponse, options, body, botReq;
 
   botResponse = cool();
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
+function postResp() {
+  var botResponse, options, body, botReq;
+
+  botResponse = 'I am alive!';
 
   options = {
     hostname: 'api.groupme.com',
