629361e1fc3d6fa0c30c7114dbc98e7e8af2af9c
jmtkt8 <jmtkt8@mail.missouri.edu>
2016-07-23 10:23:30 -0500

    Added introduction and fixed bug

--- bot.js
+++ bot.js
@@ -8,6 +8,9 @@ function respond() {
       botRegex = /^Conquistabot cool guy me$/;
 	  helloRegex = /^Conquistabot how are you$/;
 	  dogRegex = /^Conquistabot dog me$/;
+	  introRegex = /^Conquistabot introduce yourself/;
+	  helpRegex = /^Conquistabot help/;
+	  
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
@@ -19,6 +22,10 @@ function respond() {
   }else if(request.text && dogRegex.test(request.text)){
     this.res.writeHead(200);
     dogMe();
+    this.res.end();
+  }else if(request.text && introRegex.test(request.text)){
+    this.res.writeHead(200);
+    postIntro();
     this.res.end();
   }else {
     console.log("don't care");
@@ -102,7 +109,7 @@ function postResp() {
 function dogMe() {
   var botResponse, options, body, botReq, rand;
 
-  rand = Math.floor((Math.random() * 3) + 1);
+  rand = Math.floor((Math.random() * 3) + 0);
   dogs = [
 	'https://media.giphy.com/media/Llhp3CviKCKWc/giphy.gif',
 	'https://media.giphy.com/media/OsVHDytNJNQ7m/giphy.gif',
@@ -110,6 +117,41 @@ function dogMe() {
   ]
   
   botResponse = dogs[rand];
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postIntro() {
+  var botResponse, options, body, botReq;
+
+  botResponse = 'I am the ACE of this chat! I live for this chat, I breathe for this chat, and I DIE FOR MY FRAT! Type the command "Conquistabot help" to see what I can do';
 
   options = {
     hostname: 'api.groupme.com',
